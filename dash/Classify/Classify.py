acac897cb80c268043a2125f027126b5e5e20187
XinLiu92 <xliu0410@gmail.com>
2019-03-23 16:04:48 -0400

    Add classify sequence method

--- dash/Classify/Classify.py
+++ dash/Classify/Classify.py
@@ -10,6 +10,32 @@ def processQuery(query,distance):
 	tree.add(query,distance)
 
 
+#total classify sequence
+def classify_sequence(totalNodes):
+	totalNodes.pop("_root")
+	taxon = ''
+	top_score = 0.0
+	#k is name in string type, v is node instance
+	for k,v in totalNodes.items():
+		distanceList = v.getDistance()
+		score = max([float(x) for x in distanceList])
+		score = statistics.median([float(x) for x in distanceList])
+		print(k,score,sorted(distanceList,reverse=True))
+		if score > top_score:
+			top_score = score
+			taxon = k
+
+	return taxon
+
+def classify_sequence(name):
+	distanceList = tree.getDistanceByName(name)
+	taxon = ''
+	top_score = 0.0
+	
+
+
+
+
 def main():
 
 	# if sys.argv[1] == "-help" or sys.argv[1] == "-h" or sys.argv[1] == "-H" :
@@ -26,7 +52,10 @@ def main():
 		original.append(query)
 		processQuery(query,distance)
 
-	print(tree.getDistanceByName("Eukaryota"))
+	#print(tree.getDistanceByName("Eukaryota"))
+	totalNodes = {}
+	totalNodes = tree.getTotalNodes()
+	classify_sequence(totalNodes)
 
 	#tree.printTree()
 
