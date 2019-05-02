692410bb7f62299df4b7e45945b2a6afb4def7dd
XinLiu92 <xliu0410@gmail.com>
2019-05-02 15:08:08 -0400

    print tree

--- dash/Classify/Tree.py
+++ dash/Classify/Tree.py
@@ -77,9 +77,10 @@ class Tree:
         node = self.totalNodes.get(nodeName)
         node.addLeaf(leafName, leafNode)
 
+
     # add node to the tree. argument include the query list and the query list's distance
     def add(self,query,distance):
-        self.addHelper(0, query, self._root, distance)
+        self.addHelper(0,query,self._root,distance)
         querySize = len(query)
         # exe = ThreadPoolExecutor(max_workers=1)
         global leafName
@@ -141,7 +142,7 @@ class Tree:
             else:
                 node = childrenList.get(query[index])
                 childrenList = node.getChildren()
-                index += 1
+                index +=1
         return True
 
 
@@ -163,26 +164,6 @@ class Tree:
             print("No such element")
         else:
             return node.getDistance()
-
-    def getNodeByName(self, tax_path):
-        cur = ''
-        cur_node = self._root
-        index = 0
-        while cur != tax_path[-1]:
-            cur = tax_path[index]
-            cur_node = cur_node.getChildren()[cur]
-            index += 1
-        print (cur_node.getName())
-        return cur_node
-
-
-        node = self.totalNodes[name]
-        if node is None:
-            print("No such element")
-        else:
-            print ('THIS NODE', node)
-            return node
-
 
     # level order traversal. To go throught every node,
     # print out the children name which has highest distance value
@@ -219,11 +200,8 @@ class Tree:
         maxDistance = 0
         path = []
         distance = []
-<<<<<<< HEAD
         nodeLookUpMap = {}
 
-=======
->>>>>>> 5d837767e7c7b45f1d1cdb43c2ddf92be854cf0e
         while len(node.getChildren()) != 0:
             # print(path);
             # print(len(node.getChildren()))
@@ -234,13 +212,14 @@ class Tree:
                     maxDistance = v.getMaxDistance()
 
             path.append(node.getName())
+            nodeLookUpMap[node.getName()] = node
             distance.append(maxDistance)
             maxDistance = 0
         # print(node.getName())
 
         print(path);
         # print(distance)
-        return path
+        return path,nodeLookUpMap
 
     def generateLevel(self):
         q = Queue()
@@ -325,7 +304,7 @@ class Tree:
                                     pickedList[k] = v
                                     left -= 1
                             elif childNode.leafSize() < tmp:
-                                left -= childNode.leafSize()
+                                left -= childNode.leafSize
 
                                 # re-assgin
                                 if count > 0:
@@ -353,7 +332,7 @@ class Tree:
                                 left -= 1
 
                         elif childNode.leafSize() < tmp:
-                            left -= childNode.leafSize()
+                            left -= childNode.leafSize
 
                             #re-assgin
 
@@ -386,8 +365,6 @@ class Tree:
 
     def findWinnerNode(self,leafDir):
 
-
-
         return None
 
     def findRightParent(self,root,leaf):
@@ -400,26 +377,16 @@ class Tree:
         return parent
 
 
-    def findPath(self, N):
+    def findPath(self):
         path = {}
         root = self._root
 
+        #is the N fixed?
+        N = 15
+        while root.getLevel() != self.getLevel()-2 :
+            leafDir = self.randomPickHelper(root,N)
 
-        while root.getLevel() != self.getLevel()-2:
-            leafDir = self.randomPickHelper(root, N)
-
-            # genome_list = [path1, path2]
-            #
-            # classifiction_tree = Tree()
-            #
-            # genome_list = [path1, path2]
-            #
-            # for genome in genome_list:
-            #     distance, taxonomy = BLAST.blast_file(target_genome_path, genome)
-            #     processQuery(reduced_taxonomy, distance, cur_tree)
-            #
-
-            winnerNode = self.findWinnerNode(classifiction_tree)
+            winnerNode = self.findWinnerNode(leafDir)
 
             #find the parent right under root
             parentNode = self.findRightParent(root,winnerNode)
@@ -430,3 +397,52 @@ class Tree:
 
 
         return path
+
+
+    #reference:https://github.com/clemtoy/pptree
+    def print_tree(self,current_node, childattr='_childrenList', nameattr='_name', indent='', last='updown'):
+        if hasattr(current_node, nameattr):
+            name = lambda node: getattr(node, nameattr)
+        else:
+            name = lambda node: str(node)
+
+        children = lambda node: getattr(node, childattr)
+        nb_children = lambda node: sum(nb_children(child) for child in children(node)) + 1
+        size_branch = {child: nb_children(child) for child in children(current_node)}
+
+        """ Creation of balanced lists for "up" branch and "down" branch. """
+        up = sorted(children(current_node), key=lambda node: nb_children(node))
+        down = []
+        while up and sum(size_branch[node] for node in down) < sum(size_branch[node] for node in up):
+            down.append(up.pop())
+
+        """ Printing of "up" branch. """
+        for child in up:
+            next_last = 'up' if up.index(child) is 0 else ''
+            next_indent = '{0}{1}{2}'.format(indent, ' ' if 'up' in last else '│', ' ' * len(name(current_node)))
+            self.print_tree(child, childattr, nameattr, next_indent, next_last)
+
+        """ Printing of current node. """
+        if last == 'up':
+            start_shape = '┌'
+        elif last == 'down':
+            start_shape = '└'
+        elif last == 'updown':
+            start_shape = ' '
+        else:
+            start_shape = '├'
+
+        if up:
+            end_shape = '┤'
+        elif down:
+            end_shape = '┐'
+        else:
+            end_shape = ''
+
+        print('{0}{1}{2}{3}'.format(indent, start_shape, name(current_node), end_shape))
+
+        """ Printing of "down" branch. """
+        for child in down:
+            next_last = 'down' if down.index(child) is len(down) - 1 else ''
+            next_indent = '{0}{1}{2}'.format(indent, ' ' if 'down' in last else '│', ' ' * len(name(current_node)))
+            self.print_tree(child, childattr, nameattr, next_indent, next_last)
